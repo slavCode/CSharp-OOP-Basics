549220517b16a9e6c9b146fa0c87b928beb18757
Slav Tomov <stomow@gmail.com>
2017-09-04 21:06:21 +0300

    Encapsulation

---
+++ 02-Encapsulation/EncapsulationLab/04.FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var lines = int.Parse(Console.ReadLine());
+        var team = new Team("Team");
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            var person = new Person(cmdArgs[0],
+                                    cmdArgs[1],
+                                    int.Parse(cmdArgs[2]),
+                                    double.Parse(cmdArgs[3]));
+
+            team.AddPlayer(person);
+        }
+
+        Console.WriteLine($"First team have {team.FirstTeam.Count} players");
+        Console.WriteLine($"Reserve team have {team.ReserveTeam.Count} players");
+    }
+}
